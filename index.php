38f6201f1b4ab0e9ba4be7f55b05cc23b69271fa
Leo <leo3110.souza@gmail.com>
2021-12-03 19:03:00 -0300

    I tried 3/11

--- index.php
+++ index.php
@@ -4,15 +4,15 @@ include 'script/function.php';
 include 'GUI/header.php';
 include 'GUI/leftMenu.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  include_once 'Object/user.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { //Login
+  include 'Object/user.php';
   $u = new User();
   $u->checkUser();
   include 'GUI/home.php';
 }
 if (isset($_GET['op'])&&($_GET['op']=='clean'||$_GET['op']=='logout')) {
   clean();
-  include 'GUI/home.php';
+  include 'GUI/ficha.php';
 }
 elseif (isset($_GET['tela'])&&checkTela($_GET['tela'])) {
   $_SESSION['op']=$_GET['tela'];
