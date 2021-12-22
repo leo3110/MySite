54cdf5b10e79f0fb4c6e6fc77ca16d31d17b1551
Leo <leo3110.souza@gmail.com>
2021-12-22 17:42:21 -0300

    chega de GET, funcionando com AJAX

--- index.php
+++ index.php
@@ -3,16 +3,11 @@ session_start();
 include 'script/function.php';
 include 'GUI/header.php';
 
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { //Login
   include 'Object/user.php';
   $u = new User();
   $u->checkUser();
   include 'GUI/home.php';
-}
-if (isset($_GET['tela']) && $_GET['tela'] == "tk" && isset($_GET['vacilo'])) {
-	TKManda();
-	unset($_GET['vacilo']);
 }
 if (isset($_GET['op'])&&($_GET['op']=='clean'||$_GET['op']=='logout')) {
   clean();
--- script/function.php
+++ script/function.php
@@ -1,5 +1,10 @@
 <?php
-define('tela', ['login','home','ficha','tk']);
+define('tela', ['home','ficha','tk']);
+function LeftMenu(){
+  foreach (tela as $key) {
+    echo "<a href='?tela=$key'><li class=''>".ucfirst($key)."</li></a>";
+  }
+}
 function checkEqual($a,$b){
 	if (isset($a)&&$a==$b) {
 		return true;
@@ -29,15 +34,4 @@ function TKBusca(){
 	foreach ($json as $key => $value) {
 		echo "document.getElementById('$key').innerHTML = '$value';	";
 	}
-}
-function TKManda(){
-	$a = $_GET['vacilo'];
-	$bancoR = fopen("TK/db/contagem.json","r") or die("erro ao abrir o banco");
-	$json = fread($bancoR,filesize("TK/db/contagem.json"));
-	$objson = json_decode($json);
-	$objson->$a = $objson->$a+1;
-	$json = json_encode($objson);
-	fclose($bancoR);
-	$bancoE = fopen("TK/db/contagem.json","w") or die("erro ao abrir o banco");
-	fwrite($bancoE,$json);
 }
