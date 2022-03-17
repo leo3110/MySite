f4775a91dc7d9f64e27c7d52bb025e85c34aab04
Leo <leo3110.souza@gmail.com>
2022-03-17 17:32:01 -0300

    Seta bonitinha beta

--- GUI/header.php
+++ GUI/header.php
@@ -8,8 +8,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../Script/<?php echo $_SESSION['op'];?>.css">
     <link rel="stylesheet" type="text/css" href="../Script/css.css">
+    <link rel="stylesheet" type="text/css" href="../Script/<?php echo $_SESSION['op'];?>.css">
+    <link rel="stylesheet" type="text/css" href="../Script/<?php echo $_SESSION['op'];?>Anim.css">
     <link rel="shortcut icon" href="img/d20.png">
     <title>DE Souza</title>
 </head>
--- GUI/home.php
+++ GUI/home.php
@@ -1,23 +1,29 @@
-<div class="Home">
+<div class="OutArrow Flex CentroX CentroY">
+    <div class="Arrow"></div>
+</div>
+<div class="Welcome Flex CentroX CentroY">
+    Bem Vindo!
+</div>
+<div class="OutGrid">
   <div class="Grid">
 
-    <div class="Quadro">
+    <div class="Frame">
       <a href="/TK">
         <span>BRAWLHALLA TK</span>
       </a>
     </div>
-    <div class="Quadro">
+    <div class="Frame">
       <a href="#">
         <span>Sistema 2</span>
       </a>
     </div>
-    <div class="Quadro">
+    <div class="Frame">
       <a href="#">
         <span>Sistema 3</span>
       </a>
     </div>
-    <div class="Quadro">
-      <a href="/Me">
+    <div class="Frame">
+      <a href="#">
         <span>About Me</span>
       </a>
     </div>
