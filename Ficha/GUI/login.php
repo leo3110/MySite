f2417454750db36f3774ac5c8e2849174a26e304
Leo <leo3110.souza@gmail.com>
2022-03-23 17:28:24 -0300

    login cru

--- Ficha/GUI/login.php
+++ Ficha/GUI/login.php
@@ -1,3 +1,8 @@
-<div class="Login Flex Centro">
-    teste
+<div class="OutLogin Flex CentroY">
+    <div class="Login">
+        <form class="Form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <label for="Id">ID:</label> <input type="text" name="Id" value="" id="Id">
+            <label for="Pass">Senha:</label> <input type="password" name="Pass" value="" id="Pass">
+        </form>
+    </div>
 </div>
