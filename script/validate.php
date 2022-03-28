f74e2cdbcac55c585bb46a5937d6019422a1d19d
Leo <leo3110.souza@gmail.com>
2022-03-28 17:31:05 -0300

    login servido.

--- script/validate.php
+++ script/validate.php
@@ -5,13 +5,18 @@ function clean($c){
     $c = htmlspecialchars($c);
     return $c;
 }
-function checkForm() {
-    $a = clean($_POST['Id']);
-    $b = clean($_POST['Pass']);
-    $bancoR = fopen('db/banco.json','r') or die ('erro ao logar');
-    $json = fread($bancoR,filesize('db/banco.json'));
-    $objson = json_decode($json);
-    foreach ($objson as $key) {
-        echo $key->Id;
+$a = clean($_POST['Id']);
+$b = clean($_POST['Pass']);
+$bancoR = fopen('db/banco.json','r') or die ('erro ao logar');
+$json = fread($bancoR,filesize('db/banco.json'));
+$objson = json_decode($json, true);
+foreach ($objson as $key => $value) {
+    if ($key == $a && $value == $b) {
+        $_SESSION['Login']='ok';
+        return $_SESSION['Logado']=$key;
+    }
+    else {
+        unset($_SESSION['Logado']);
+        return $_SESSION['Login']='erro';
     }
 }
