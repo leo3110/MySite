0f08597b85f788cd26576443d05d42f009f2c026
Leo <leo3110.souza@gmail.com>
2022-04-27 17:30:13 -0300

    info limpa

--- TK/script/api.php
+++ TK/script/api.php
@@ -10,23 +10,39 @@
 $player = fopen("db/apiBase.json",'r') or die("erro ao abrir arquivo base");
 $player = fread($player, filesize("db/apiBase.json"));
 $player = json_decode($player,true);
-foreach ($player as $info => $valor) {
-    if ($info != "clan" && $info != "legends"){
-        echo "$info: $valor<br>";
+function limpa($limpa){
+    if (gettype($limpa) == "double") {
+        $limpa = $limpa*100;
+        $limpa = round($limpa)."%";
+        return $limpa;
     }
+    else if (gettype($limpa) == "string") {
+        return ucfirst($limpa);
+    }
+    else {
+        return $limpa;
+    }
+}
+function wtf($chave){
+
+}
+foreach ($player as $info => $valor) {
     if ($info =='legends') {
         for ($i=0; $i < sizeof($valor); $i++) {
             echo "<div class='Lenda'>";
-            echo "<h3 class='LendaNome'>".$valor["$i"]["legend_name_key"]."</h3>";
+            echo "<h1 class='LendaNome'>".$valor["$i"]["legend_name_key"]."</h1>";
             echo "<div class='InfoLenda Flex Wrap'>";
             foreach ($valor["$i"] as $lenda => $stat) {
                 if ($lenda != "legend_name_key") {
-                    echo "<div class='InfoLendaItem'>$lenda: $stat</div>";
+                    echo "<div class='InfoLendaItem Flex Centro'>$lenda: </div>";
                 }
             }
             echo "</div>";
             echo "</div>";
         }
+    }
+    if ($info != "clan" && $info != "legends"){
+        echo "$info: $valor<br>";
     }
     if ($info == "clan") {
         echo "Clan {<br>";
